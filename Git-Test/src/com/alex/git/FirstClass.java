191b4b07891a84ba8417957f93f47c4e02c863cb
Alexvan <alex@.com>
2020-07-23 16:05:14 +0800

    修改FristClass

--- Git-Test/src/com/alex/git/FirstClass.java
+++ Git-Test/src/com/alex/git/FirstClass.java
@@ -3,11 +3,13 @@ package com.alex.git;
 
 public class FirstClass {
     public static void main(String[] args) {
+        //hello
         System.out.println("hello");
     }
 
     public void tset(){
         int i = 0;
         i += 1;
+        System.out.println(i);
     }
 }
