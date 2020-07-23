fa56485614499f87df10b783ffc08ea316ff6de4
Alexvan <alex@.com>
2020-07-23 16:11:46 +0800

    添加一个second类到本地

---
+++ Git-Test/src/com/alex/git/SecondClass.java
@@ -0,0 +1,12 @@
+package com.alex.git;
+
+/**
+ * 模块功能
+ *
+ * @Author 黄
+ **/
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
